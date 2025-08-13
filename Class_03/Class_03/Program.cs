11f39a9cd1f06ad2dc58fe567cc18495c2a568ba
Saleha517 <salehausman517@gmail.com>
2025-08-13 04:39:24 -0700

    Switch Case | Conditions

---
+++ Class_03/Class_03/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Username");
+            string user = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Password");
+            string pass = Console.ReadLine();
+
+            string admin = user == "saleha" && pass == 123 ? "Login successfully" : "invalid user or pass";
+
+            Console.WriteLine(admin);
+
+
+
+
+
+            Console.WriteLine("----\"Calculator\"-------");
+
+            Console.WriteLine("Enter First Number");
+            int digit1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number");
+            int digit2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Any Operator (+ , - , * , / )");
+            string opt = Console.ReadLine();
+
+            string output = Console.ReadLine();
+
+            string result = "";
+
+             switch (opt)
+            {
+                case "+":
+
+                    result = $"{digit1 - digit2}";
+
+                    break;
+
+                case "-":
+
+                    result = $"{digit1 - digit2}";
+
+                    break;
+
+                case "*":
+
+                    result = $"{digit1 * digit2}";
+
+                    break;
+
+                case "/":
+
+                    result = $"{digit1 / digit2}";
+
+                    break;
+
+                default:
+                    result = $"{"Invalid Data Inserted"}";
+
+                    break;
+
+                    
+              
+            }
+
+            Console.WriteLine($"Your Result is : {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
